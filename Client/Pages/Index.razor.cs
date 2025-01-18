7c4eacf6f9fc7ad9dbf0407007b7488a14dab674
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2025-01-18 16:55:35 +0530

    Update

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Net.Http;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
@@ -14,32 +15,26 @@ namespace ERP.Client.Pages
 {
     public partial class Index
     {
-        [Inject]
-        protected IJSRuntime JsRuntime { get; set; }
+        [Inject] protected IJSRuntime JsRuntime { get; set; }
 
-        [Inject]
-        protected NavigationManager NavigationManager { get; set; }
+        [Inject] protected NavigationManager NavigationManager { get; set; }
 
-        [Inject]
-        protected DialogService DialogService { get; set; }
+        [Inject] protected DialogService DialogService { get; set; }
 
-        [Inject]
-        protected TooltipService TooltipService { get; set; }
+        [Inject] protected TooltipService TooltipService { get; set; }
 
-        [Inject]
-        protected ContextMenuService ContextMenuService { get; set; }
+        [Inject] protected ContextMenuService ContextMenuService { get; set; }
 
-        [Inject]
-        protected NotificationService NotificationService { get; set; }
+        [Inject] protected NotificationService NotificationService { get; set; }
 
-        [Inject]
-        protected SecurityService Security { get; set; }
-        
+        [Inject] protected SecurityService Security { get; set; }
+
         void OnSeriesClick(SeriesClickEventArgs args)
         {
-            NotificationService.Notify(new NotificationMessage() { Severity = NotificationSeverity.Info, Summary = "Series Click", Detail = $"Title: {args.Title}" });
+            NotificationService.Notify(new NotificationMessage()
+                { Severity = NotificationSeverity.Info, Summary = "Series Click", Detail = $"Title: {args.Title}" });
         }
-        
+
         class MastersByType
         {
             public string Type { get; set; }
@@ -75,6 +70,151 @@ namespace ERP.Client.Pages
             },
         ];
 
+        /// <summary>
+        /// DataItem class
+        /// </summary>
+        class DataItem
+        {
+            public string Date { get; set; }
+            public double Revenue { get; set; }
+        }
+
+        string FormatAsUSD(object value)
+        {
+            return ((double)value).ToString("C0", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+
+        DataItem[] revenue2023 = new DataItem[]
+        {
+            new DataItem
+            {
+                Date = "Jan",
+                Revenue = 234000
+            },
+            new DataItem
+            {
+                Date = "Feb",
+                Revenue = 269000
+            },
+            new DataItem
+            {
+                Date = "Mar",
+                Revenue = 233000
+            },
+            new DataItem
+            {
+                Date = "Apr",
+                Revenue = 244000
+            },
+            new DataItem
+            {
+                Date = "May",
+                Revenue = 214000
+            },
+            new DataItem
+            {
+                Date = "Jun",
+                Revenue = 253000
+            },
+            new DataItem
+            {
+                Date = "Jul",
+                Revenue = 274000
+            },
+            new DataItem
+            {
+                Date = "Aug",
+                Revenue = 284000
+            },
+            new DataItem
+            {
+                Date = "Sept",
+                Revenue = 273000
+            },
+            new DataItem
+            {
+                Date = "Oct",
+                Revenue = 282000
+            },
+            new DataItem
+            {
+                Date = "Nov",
+                Revenue = 289000
+            },
+            new DataItem
+            {
+                Date = "Dec",
+                Revenue = 294000
+            }
+        };
+
+        DataItem[] revenue2024 = new DataItem[]
+        {
+            new DataItem
+            {
+                Date = "Jan",
+                Revenue = 334000
+            },
+            new DataItem
+            {
+                Date = "Feb",
+                Revenue = 369000
+            },
+            new DataItem
+            {
+                Date = "Mar",
+                Revenue = 333000
+            },
+            new DataItem
+            {
+                Date = "Apr",
+                Revenue = 344000
+            },
+            new DataItem
+            {
+                Date = "May",
+                Revenue = 314000
+            },
+            new DataItem
+            {
+                Date = "Jun",
+                Revenue = 353000
+            },
+            new DataItem
+            {
+                Date = "Jul",
+                Revenue = 374000
+            },
+            new DataItem
+            {
+                Date = "Aug",
+                Revenue = 384000
+            },
+            new DataItem
+            {
+                Date = "Sept",
+                Revenue = 373000
+            },
+            new DataItem
+            {
+                Date = "Oct",
+                Revenue = 382000
+            },
+            new DataItem
+            {
+                Date = "Nov",
+                Revenue = 389000
+            },
+            new DataItem
+            {
+                Date = "Dec",
+                Revenue = 394000
+            }
+        };
+
+        /// <summary>
+        /// Transaction class
+        /// </summary>
         class Txn
         {
             public string Id { get; set; }
