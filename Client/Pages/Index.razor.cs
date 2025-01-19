eea0360afc658e961799bf15263e3d082c35b70d
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2025-01-19 17:13:20 +0530

    Update

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -73,7 +73,7 @@ namespace ERP.Client.Pages
         /// <summary>
         /// DataItem class
         /// </summary>
-        class DataItem
+        class DataItem1
         {
             public string Date { get; set; }
             public double Revenue { get; set; }
@@ -84,128 +84,128 @@ namespace ERP.Client.Pages
             return ((double)value).ToString("C0", CultureInfo.CreateSpecificCulture("en-US"));
         }
 
-        DataItem[] revenue2023 = new DataItem[]
+        DataItem1[] revenue2023 = new DataItem1[]
         {
-            new DataItem
+            new DataItem1
             {
                 Date = "Jan",
                 Revenue = 234000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Feb",
                 Revenue = 269000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Mar",
                 Revenue = 233000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Apr",
                 Revenue = 244000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "May",
                 Revenue = 214000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Jun",
                 Revenue = 253000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Jul",
                 Revenue = 274000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Aug",
                 Revenue = 284000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Sept",
                 Revenue = 273000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Oct",
                 Revenue = 282000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Nov",
                 Revenue = 289000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Dec",
                 Revenue = 294000
             }
         };
 
-        DataItem[] revenue2024 = new DataItem[]
+        DataItem1[] revenue2024 = new DataItem1[]
         {
-            new DataItem
+            new DataItem1
             {
                 Date = "Jan",
                 Revenue = 334000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Feb",
                 Revenue = 369000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Mar",
                 Revenue = 333000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Apr",
                 Revenue = 344000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "May",
                 Revenue = 314000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Jun",
                 Revenue = 353000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Jul",
                 Revenue = 374000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Aug",
                 Revenue = 384000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Sept",
                 Revenue = 373000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Oct",
                 Revenue = 382000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Nov",
                 Revenue = 389000
             },
-            new DataItem
+            new DataItem1
             {
                 Date = "Dec",
                 Revenue = 394000
@@ -295,6 +295,49 @@ namespace ERP.Client.Pages
                 Date = DateTime.Now,
                 Total = 40000
             },
+            new Txn
+            {
+                Id = "11",
+                Name = "Mail-Order",
+                Date = DateTime.Now,
+                Total = 50000
+            },
+            new Txn
+            {
+                Id = "12",
+                Name = "Others",
+                Date = DateTime.Now,
+                Total = 80000
+            }
         ];
+        
+        class DataItem2
+        {
+            public string Quarter { get; set; }
+            public double Revenue { get; set; }
+        }
+
+        DataItem2[] revenue = new DataItem2[] {
+            new DataItem2
+            {
+                Quarter = "Q1",
+                Revenue = 30000
+            },
+            new DataItem2
+            {
+                Quarter = "Q2",
+                Revenue = 40000
+            },
+            new DataItem2
+            {
+                Quarter = "Q3",
+                Revenue = 50000
+            },
+            new DataItem2
+            {
+                Quarter = "Q4",
+                Revenue = 80000
+            },
+        };
     }
 }
